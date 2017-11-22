0862488915e7afed1e1e791d14f9d39bda070095
kusole <lt19942013@163.com>
2017-11-22 22:46:04 +0800

    first commit

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,10 +3,10 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 # Create your views here.
-from .models import UserProfile,EmailVerifyRecord
+from .models import UserProfile, EmailVerifyRecord
 from django.views.generic.base import View
 from django.contrib.auth.hashers import make_password
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, ForgetForm, ModifyPwdForm
 from utils.email_send import send_register_email
 
 
@@ -42,19 +42,6 @@ class LoginView(View):
             return render(request, "login.html", {"login_form": login_form})
 
 
-# def user_login(request):
-#     if request.method == "POST":
-#         user_name = request.POST.get("username","")
-#         pass_word = request.POST.get("password","")
-#         user = authenticate(username=user_name, password=pass_word)
-#         if user is not None:
-#             login(request, user)
-#             return render(request, "index.html")
-#         else:
-#             return render(request, "login.html",{"msg":"用户名或密码错误"})
-#     elif request.method == "GET":
-#         return render(request, "login.html",{})
-
 class RegisterView(View):
     def get(self, request):
         register_form = RegisterForm()
@@ -64,6 +51,8 @@ class RegisterView(View):
         register_form = RegisterForm(request.POST)
         if register_form.is_valid():
             user_name = request.POST.get("email", "")
+            if UserProfile.objects.filter(email=user_name):
+                return render(request, "register.html", {"register_form": register_form, "msg": "用户已经存在"})
             pass_word = request.POST.get("password", "")
             user_profile = UserProfile()
             user_profile.username = user_name
@@ -77,6 +66,7 @@ class RegisterView(View):
         else:
             return render(request, "register.html", {"register_form": register_form})
 
+
 class ActiveUserView(View):
     def get(self, request, active_code):
         all_recodes = EmailVerifyRecord.objects.filter(code=active_code)
@@ -86,4 +76,54 @@ class ActiveUserView(View):
                 user = UserProfile.objects.get(email=email)
                 user.is_active = True
                 user.save()
-        return render(request, "login.html",{})
+        else:
+            pass
+        return render(request, "login.html", {})
+
+
+class ForgetPwdView(View):
+    def get(self, request):
+        forget_form = ForgetForm()
+        return render(request, "forgetpwd.html", {'forget_form': forget_form})
+
+    def post(self, request):
+        forget_form = ForgetForm(request.POST)
+        if forget_form.is_valid():
+            email = request.POST.get("email", "")
+            send_register_email(email, "forget")
+            return render(request, "send_success.html", {})
+        else:
+            return render(request, "forgetpwd.html", {'forget_form': forget_form})
+
+
+class ResetView(View):
+    def get(self, request, active_code):
+        all_recodes = EmailVerifyRecord.objects.filter(code=active_code)
+        if all_recodes:
+            for recode in all_recodes:
+                email = recode.email
+                code = recode.code
+                return render(request, "password_reset.html", {"email": email})
+        else:
+            return render(request, "active_fail.html", {})
+        return render(request, "login.html", {})
+
+class ModifyPwdView(View):
+    def post(self, request):
+        modify_form = ModifyPwdForm(request.POST)
+        if modify_form.is_valid():
+            pwd1 = request.POST.get("password1", "")
+            pwd2 = request.POST.get("password2", "")
+            email = request.POST.get("email", "")
+            if pwd1 != pwd2:
+                return render(request,"password_reset.html",{"email": email,"msg":"密码不一致"})
+            user = UserProfile.objects.get(email=email)
+            user.password=make_password(pwd1)
+            user.save()
+
+            return render(request,"login.html")
+        else:
+            email = request.POST.get("email", "")
+            return render(request, "password_reset.html", {"email": email, "modify_form": modify_form})
+
+
--- mkonline/urls.py
+++ mkonline/urls.py
@@ -13,18 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
-from users.views import LoginView,RegisterView,ActiveUserView
+from users.views import LoginView, RegisterView, ActiveUserView, ForgetPwdView,ResetView,ModifyPwdView
 import xadmin
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name="index.html"),name="index"),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name="index"),
     url(r'^login/$', LoginView.as_view(), name="login"),
-    url(r'^register/$',RegisterView.as_view(),name="register"),
+    url(r'^register/$', RegisterView.as_view(), name="register"),
 
     url(r'^captcha/', include('captcha.urls')),
-    url(r'^active/(?P<active_code>.*)/$', ActiveUserView.as_view(), name="active"),
+    url(r'^active/(?P<active_code>.*)/$', ActiveUserView.as_view(), name="user_active"),
+    url(r'^forgetpwd/', ForgetPwdView.as_view(), name="forget_pwd"),
+    url(r'^reset/(?P<active_code>.*)/$', ResetView.as_view(), name="reset_pwd"),
+    url(r'^modify_pwd/$', ModifyPwdView.as_view(), name="modify_pwd"),
 ]
